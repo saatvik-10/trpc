d7224a2c88805bdd4069e2835ee225db176ff9b2
saatvik-10 <saatvikmadan529@gmail.com>
2026-02-27 19:36:21 +0530

    client auth header

--- client/index.ts
+++ client/index.ts
@@ -5,6 +5,11 @@ export const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
       url: 'http://localhost:8000',
+      async headers() {
+        return {
+          Authorization: 'Bearer 123',
+        };
+      },
     }),
   ],
 });
